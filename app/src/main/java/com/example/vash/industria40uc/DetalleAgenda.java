1589df0803f3525795d44aa22b3c596c2fc1c611
Gino <gino_gjtm@hotmail.com>
2019-11-01 10:02:09 -0500

    Configuracion Activity

---
+++ app/src/main/java/com/example/vash/industria40uc/DetalleAgenda.java
@@ -0,0 +1,13 @@
+package com.example.vash.industria40uc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalleAgenda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_agenda);
+    }
+}
